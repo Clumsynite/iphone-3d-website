15abc1ae0f590ae5ee7ff515a1d63e2f11a6bf6f
Rishabh Pathak <clusmsyknight@gmail.com>
2023-05-21 18:30:50 +0530

    added scroll to inside jumbotron
    scroll to sound section

--- src/components/Jumbotron.jsx
+++ src/components/Jumbotron.jsx
@@ -3,6 +3,17 @@ import Iphone from "../assets/images/iphone-14.jpg";
 import HoldingIphone from "../assets/images/iphone-hand.png";
 
 function Jumbotron() {
+  const handleLearnMore = () => {
+    const element = document.querySelector(".sound-section");
+    if (element) {
+      const { top } = element.getBoundingClientRect();
+      window.scrollTo({
+        top,
+        left: 0,
+        behavior: "smooth",
+      });
+    }
+  };
   return (
     <div className="jumbotron-section wrapper">
       <h2 className="title">New</h2>
@@ -14,7 +25,9 @@ function Jumbotron() {
           <button className="button">Buy</button>
         </li>
         <li>
-          <a className="link">Learn more</a>
+          <a className="link" onClick={handleLearnMore}>
+            Learn more
+          </a>
         </li>
       </ul>
       <img className="iphone-img" src={HoldingIphone} alt="Iphone" />
